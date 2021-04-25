c92b85689026d65163017c63c319fcf14b49400f
Alon Soffer <alon.soffer@mail.huji.ac.il>
2021-04-25 12:00:38 +0300

    CalculateRootService implamented and passed test

--- app/src/main/java/exercise/find/roots/CalculateRootsService.java
+++ app/src/main/java/exercise/find/roots/CalculateRootsService.java
@@ -6,20 +6,101 @@ import android.util.Log;
 
 public class CalculateRootsService extends IntentService {
 
-
-  public CalculateRootsService() {
-    super("CalculateRootsService");
-  }
-
-  @Override
-  protected void onHandleIntent(Intent intent) {
-    if (intent == null) return;
-    long timeStartMs = System.currentTimeMillis();
-    long numberToCalculateRootsFor = intent.getLongExtra("number_for_service", 0);
-    if (numberToCalculateRootsFor <= 0) {
-      Log.e("CalculateRootsService", "can't calculate roots for non-positive input" + numberToCalculateRootsFor);
-      return;
+    @Override
+    public void sendBroadcast(Intent intent) {
+        super.sendBroadcast(intent);
     }
+
+
+    public CalculateRootsService() {
+        super("CalculateRootsService");
+    }
+
+    /**
+     * checks if 20 seconds passed
+     * @param timeStartMs time when service began
+     * @return true if 20 have passed since service began
+     */
+    private boolean timePassed(long timeStartMs)
+    {
+        long currTimeMs = System.currentTimeMillis();
+        return CalculateRootsService.msToSec(currTimeMs - timeStartMs) >= 20;
+    }
+
+    /**
+     * checks if given number is prime
+     * @param num number to check
+     * @param timeStartMs time when service began
+     * @return 1 if number is prime, 0 if not, and -1 if check took more than 20 seconds
+     */
+    private int isPrimeNumber(long num, long timeStartMs)
+    {
+        if (num <= 1 || num % 2 == 0 || num % 3 == 0){
+            return 0;
+        }
+        if (num == 2 || num == 3){
+            return 0;
+        }
+
+        for (int i = 5; i*i <= num; i += 6)
+        {
+            if (num % i == 0 || num % (i+2) == 0)
+            {
+                return 0;
+            }
+
+            if (timePassed(timeStartMs)) // TODO: should check every iteration or every X iterations?
+            {
+                return -1;
+            }
+        }
+        return 1;
+    }
+
+    /**
+     * finds roots for a non-prime number, and puts them in roots
+     * @param num number to find roots for
+     * @param roots long array [2] where roots will be. if calculation takes more than 20 seconds - root[0] = -1
+     * @param timeStartMs time in ms when service began
+     */
+    private void calculateRoots(long num, long[] roots, long timeStartMs)
+    {
+        int i = 2;
+        while (true)
+        {
+            if (num % i == 0) // i is root of num
+            {
+                roots[0] = i;
+                roots[1] = num / i;
+                return;
+            }
+            if (timePassed(timeStartMs)) // TODO: should check every iteration or every X iterations?
+            {
+                roots[0] = -1;
+                roots[1] = System.currentTimeMillis();
+                return;
+            }
+            i++;
+        }
+    }
+
+    /**
+     * converts ms to sec
+     */
+    private static long msToSec(long timeInMs)
+    {
+        return timeInMs*1000; //TODO check conversion
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent == null) return;
+        long timeStartMs = System.currentTimeMillis();
+        long numberToCalculateRootsFor = intent.getLongExtra("number_for_service", 0);
+        if (numberToCalculateRootsFor <= 0) {
+            Log.e("CalculateRootsService", "can't calculate roots for non-positive input" + numberToCalculateRootsFor);
+            return;
+        }
     /*
     TODO:
      calculate the roots.
@@ -39,7 +120,41 @@ public class CalculateRootsService extends IntentService {
        for input "30", roots can be (3, 10) or (2, 15) or other options
        for input "17", roots are (17, 1)
        for input "829851628752296034247307144300617649465159", after 20 seconds give up
-
      */
-  }
+        int isPrime = isPrimeNumber(numberToCalculateRootsFor, timeStartMs);
+        Intent outIntent = new Intent();
+        switch (isPrime)
+        {
+            case 1: // prime
+                outIntent.setAction("found_roots");
+                outIntent.putExtra("original_number", numberToCalculateRootsFor);
+                outIntent.putExtra("root1", numberToCalculateRootsFor);
+                outIntent.putExtra("root2", 1);
+                break;
+            case -1: // took more than 20 seconds to check if prime
+                long timeWeStoppedMs = System.currentTimeMillis();
+                outIntent.setAction("stopped_calculations");
+                outIntent.putExtra("original_number", numberToCalculateRootsFor);
+                outIntent.putExtra("time_until_give_up_seconds", CalculateRootsService.msToSec(timeWeStoppedMs - timeStartMs));
+                break;
+            case 0: // non-prime
+                long[] roots = new long[2];
+                calculateRoots(numberToCalculateRootsFor, roots, timeStartMs);
+                if (roots[0] == -1) // took more than 20 seconds to find roots
+                {
+                    outIntent.setAction("stopped_calculations");
+                    outIntent.putExtra("original_number", numberToCalculateRootsFor);
+                    outIntent.putExtra("time_until_give_up_seconds", CalculateRootsService.msToSec(roots[1] - timeStartMs));
+                }
+                else // found roots
+                {
+                    outIntent.setAction("found_roots");
+                    outIntent.putExtra("original_number", numberToCalculateRootsFor);
+                    outIntent.putExtra("root1", roots[0]);
+                    outIntent.putExtra("root2", roots[1]);
+                }
+                break;
+        }
+        sendBroadcast(outIntent);
+    }
 }
--- app/src/test/java/exercise/find/roots/CalculateRootsServiceTest.java
+++ app/src/test/java/exercise/find/roots/CalculateRootsServiceTest.java
@@ -20,32 +20,32 @@ import static org.robolectric.Shadows.shadowOf;
 @Config(sdk = 28)
 public class CalculateRootsServiceTest extends TestCase {
 
-  @Test
-  public void when_calculatingRootsWithSuccess_then_serviceShouldSendBroadcastWithResults(){
-    // create a service
-    CalculateRootsService service = Robolectric.setupIntentService(CalculateRootsService.class);
+    @Test
+    public void when_calculatingRootsWithSuccess_then_serviceShouldSendBroadcastWithResults(){
+        // create a service
+        CalculateRootsService service = Robolectric.setupIntentService(CalculateRootsService.class);
 
-    // create intent to send to the service
-    Intent incomingIntent = new Intent();
-    incomingIntent.putExtra("number_for_service", 15L);
+        // create intent to send to the service
+        Intent incomingIntent = new Intent();
+        incomingIntent.putExtra("number_for_service", 15L);
 
-    // let service do it's magic to calculate the roots
-    service.onHandleIntent(incomingIntent);
+        // let service do it's magic to calculate the roots
+        service.onHandleIntent(incomingIntent);
 
-    // capture all broadcasts
-    List<Intent> broadcastedIntents = shadowOf(RuntimeEnvironment.application).getBroadcastIntents();
-    if (broadcastedIntents.size() != 1) {
-      fail("expecting exactly 1 broadcast to be sent by the service");
+        // capture all broadcasts
+        List<Intent> broadcastedIntents = shadowOf(RuntimeEnvironment.application).getBroadcastIntents();
+        if (broadcastedIntents.size() != 1) {
+            fail("expecting exactly 1 broadcast to be sent by the service");
+        }
+
+        // verify the broadcast that was sent by the service has all the data we expect
+        Intent broadcastFromService = broadcastedIntents.get(0);
+        assertNotNull(broadcastFromService);
+        assertEquals("found_roots", broadcastFromService.getAction());
+        long originalNumber = broadcastFromService.getLongExtra("original_number", 0);
+        long firstRoot = broadcastFromService.getLongExtra("root1", 0);
+        long secondRoot = broadcastFromService.getLongExtra("root2", 0);
+        assertEquals(15, originalNumber);
+        assertEquals(15, firstRoot * secondRoot);
     }
-
-    // verify the broadcast that was sent by the service has all the data we expect
-    Intent broadcastFromService = broadcastedIntents.get(0);
-    assertNotNull(broadcastFromService);
-    assertEquals("found_roots", broadcastFromService.getAction());
-    long originalNumber = broadcastFromService.getLongExtra("original_number", 0);
-    long firstRoot = broadcastFromService.getLongExtra("root1", 0);
-    long secondRoot = broadcastFromService.getLongExtra("root2", 0);
-    assertEquals(15, originalNumber);
-    assertEquals(15, firstRoot * secondRoot);
-  }
 }
